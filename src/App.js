0e56b94539706335d80b685974f0236b70afcccf
IsraelTech-Pro <149181214+IsraelTech-Pro@users.noreply.github.com>
2023-12-24 07:12:42 +0000

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import NavBar from './components/navbar/NavBar';
+import Search from './components/search/Search';
+
+function App() {
+  return (
+    <MuiThemeProvider>
+      <div>
+      <NavBar/>
+      <Search/>
+      </div>
+    </MuiThemeProvider>
+   
+  );
+}
+export default App;
